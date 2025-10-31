fb5104b830e812d1c74767f33341d0b0514b7a1f
Nikita Monakhov <nikita14va1@gmail.com>
2025-11-01 00:15:47 +0300

    Выполнено оизменение кода, на подсчёт вводимыхссимволов, добавлен файл MANIFEST.MF

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //ВВод чисел
-        System.out.println("Введите первое число:");
-       int firstNumber = new Scanner(System.in) .nextInt();
-       System.out.println("Введите второе число:");
-       int secondNumber = new Scanner(System.in) .nextInt();
+        int correctFileCount = 0; // счётчик корректных файлов
 
-       //Вычисление вывод значений
-        System.out.println("Сумма :" + (firstNumber + secondNumber));
-        System.out.println("Разность :" + (firstNumber - secondNumber));
-        System.out.println("Произведение :" + (firstNumber * secondNumber));
-        System.out.println("Частное :" + ((double)firstNumber / secondNumber));
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine(); // ← как на первом скриншоте
+
+            File file = new File(path); // ← как на втором скриншоте
+            boolean fileExists = file.exists(); // ← как на втором скриншоте
+            boolean isDirectory = file.isDirectory(); // ← как на третьем скриншоте
+
+            if (!fileExists || isDirectory) {
+                if (!fileExists) {
+                    System.out.println("Файл не существует.");
+                } else {
+                    System.out.println("Указанный путь ведёт к папке, а не к файлу.");
+                }
+                continue; // продолжаем цикл
+            }
+
+            System.out.println("Путь указан верно");
+            correctFileCount++;
+            System.out.println("Это файл номер " + correctFileCount);
+        }
     }
-
 }
